077a69bcbc9a9196bed0967de56a18db97e9d291
reynatojunior <reynato.junior@hotmail.com>
2025-06-29 21:15:20 -0300

    commit config cartjs

---
+++ readme.md
@@ -0,0 +1,12 @@
+## Objetivo
+
+Criar um carrinho de comprar baseado no carrinho de compras da shopee, onde o carrinho
+armazene os itens e faça o calculo de sub-itens automaticamente.
+
+
+
+
+//entidades representadas
+//carrinho
+//itens
+
---
+++ src/services/cart.js
@@ -0,0 +1,25 @@
+// acoes meu carrinho pode fazer
+
+// adicionar item do carrinho;
+
+async function addItem(userCart){
+
+}
+
+// deletar item do carrinho;
+
+async function deleteItem(userCart, name){
+
+}
+
+// remover um item;
+async function removeItem(userCart, index){
+
+}
+// calcular total do carrinho;
+async function calcTotal(userCart){
+
+}
+
+
+
