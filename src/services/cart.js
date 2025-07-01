a7633de841d110908911a052106de8ab556bc368
reynatojunior <reynato.junior@hotmail.com>
2025-07-01 20:28:55 -0300

    commit feat :: remove index by indice

--- src/services/cart.js
+++ src/services/cart.js
@@ -2,38 +2,38 @@
 
 // adicionar item do carrinho;
 
-async function addItem(userCart, item){
-    userCart.push(item);
-
+async function addItem(userCart, item) {
+  userCart.push(item);
 }
 
 // deletar item do carrinho;
 
-async function deleteItem(userCart, name){
-
-}
+async function deleteItem(userCart, name) {}
 
 // remover um item;
-async function removeItem(userCart, name){
-    const index = userCart.findIndex((item) => item.name === name);
-    if(index !== -1){
-        userCart.splice(index, 1);
-    }
+async function removeItem(userCart, index) {
+
+    const deleteIndex = index - 1;
+  if (index >= 0 && index < userCart.length) {
+    userCart.splice(index, 1);
+  }
 }
 
 // calcular total do carrinho;
-async function calcTotal(userCart){
-    console.log( userCart.reduce((total, item) => total + item.subtotal(), 0));
+async function calcTotal(userCart) {
+  console.log(userCart.reduce((total, item) => total + item.subtotal(), 0));
 }
 
 //mostrar carrinho
-async function displayCart(userCart){
-    console.log("Shopee cart list");
-    userCart.forEach((item, index) => {
-        console.log(`${index +1}. ${item.name} - R$ ${item.price} | ${item.quantity} x | Subtotal: R$ ${item.subtotal()}` );
-    })
-
+async function displayCart(userCart) {
+  console.log("Shopee cart list");
+  userCart.forEach((item, index) => {
+    console.log(
+      `${index + 1}. ${item.name} - R$ ${item.price} | ${
+        item.quantity
+      } x | Subtotal: R$ ${item.subtotal()}`
+    );
+  });
 }
 
-export {addItem, deleteItem, removeItem, calcTotal, displayCart};
-
+export { addItem, deleteItem, removeItem, calcTotal, displayCart };
