ddabb597aa6cf63044f2334b30fb7836c6401337
reynatojunior <reynato.junior@hotmail.com>
2025-06-30 21:14:06 -0300

    commit feat :: delete-Item

--- src/index.js
+++ src/index.js
@@ -7,11 +7,17 @@ const myWhishList = [];
 
 console.log("welcome to Shopee cart!");
 
-const item1 = await createItem("carrinho hotwheels Ferrari", 20.99, 7);
+const item1 = await createItem("carrinho hotwheels Ferrari", 20.99, 1);
 const item2 = await createItem("carrinho hotwheels Volks", 20.99, 4);
 
+
+//adicionando itens
 await cartService.addItem(myCart, item1);
-await cartService.addItem(myWhishList, item2);
+await cartService.addItem(myCart, item2);
+
+
+//removendo itens
+await cartService.removeItem(myCart, item2.name);
 
 await cartService.calcTotal(myCart);
 
--- src/services/cart.js
+++ src/services/cart.js
@@ -14,9 +14,13 @@ async function deleteItem(userCart, name){
 }
 
 // remover um item;
-async function removeItem(userCart, index){
-
+async function removeItem(userCart, name){
+    const index = userCart.findIndex((item) => item.name === name);
+    if(index !== -1){
+        userCart.splice(index, 1);
+    }
 }
+
 // calcular total do carrinho;
 async function calcTotal(userCart){
     console.log( userCart.reduce((total, item) => total + item.subtotal(), 0));
